0727dcff614b528de6d12739dd20fd4726888590
Colin <colingoodale@gmail.com>
2020-04-08 10:22:07 -0500

    boomer text

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function conflictSeason() {
     alert("There may beissues on the page!");
 
     $("#boom").text("")
-    $("#boom").text("The Balloon went poppy!")
+    $("#boom").text("Boomer text!")
 }
 conflictSeason();
